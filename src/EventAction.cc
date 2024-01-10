ed63eca32063d244efb665e2f3723dc6d22d48de
HungBT <hungbt1908@gmail.com>
2024-01-10 15:51:55 +0700

    add test1

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -35,48 +35,15 @@ void EventAction::EndOfEventAction(const G4Event* event)
 	// get analysis manager
 	G4AnalysisManager* man = G4AnalysisManager::Instance();
 
-	G4double newfEdep = AddGaussEnergy(fEdep);
-
-	if (newfEdep > 0)
+	if (fEdep > 0)
 	{
 		fEdep = fEdep/MeV;
 
 		// fill histograms
-		man->FillH1(0, newfEdep);
+		man->FillH1(0, fEdep);
 
 		// fill ntuple
-		man->FillNtupleDColumn(0, newfEdep);
+		man->FillNtupleDColumn(0, fEdep);
 		man->AddNtupleRow();
 	}
-	
-}
-
-G4double EventAction::AddGaussEnergy(G4double fEdep)
-{
-	G4double r1, r2, x1, x2, edepStdGaus;
-
-	r1 = r2 = 0.;
-	x1 = x2 = 0.;
-	edepStdGaus = 0.;
-
-	r1 = CLHEP::HepUniformRand();
-	r2 = CLHEP::HepUniformRand();
-
-	x1 = std::sqrt(-2.*std::log(r1));
-	x2 = std::sqrt(2.*M_PI*r2);
-	edepStdGaus = x1*x2;
-
-  	// parameters
-	G4double a = 1.56762;
-	G4double b = 0.0154;
-	G4double c = 3.3718E-04; 
-
-	G4double deltaEdep, edepResolution;
-	deltaEdep = 0.;
-	edepResolution = 0.;
-
-	deltaEdep = ((a + b*sqrt(fEdep + c*fEdep*fEdep))/2.35482)*edepStdGaus;
-	edepResolution = fEdep + deltaEdep;
-
-	return edepResolution;
 }
